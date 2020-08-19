6af79a20643b47185f34edd24053706b69dc1019
naama <0naama0@gmail.com>
2020-08-18 22:46:48 -0400

    added s

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl;
 
 const apiUrls = {
-    production: "http://mood-verse-app.herokuapp.com",
+    production: "https://mood-verse-app.herokuapp.com",
     development: "http://localhost:3000"
 }
 
